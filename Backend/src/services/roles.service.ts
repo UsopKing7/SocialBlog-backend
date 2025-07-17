2b22152924907d7914c8e3c997aed9cfc5cadaed
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-07-17 01:56:53 -0400

    agregamos tabla token en la base de datos ORM

--- Backend/src/services/roles.service.ts
+++ Backend/src/services/roles.service.ts
@@ -1,11 +1,19 @@
 import { roleRepository } from '../repositories/roles.repository'
+import { userRepository } from '../repositories/user.repository'
 
-export const roleService = {
-  create: async (role: { name_role: string }) => {
-    const rolExiste = await roleRepository.findRole(role.name_role)
-    if (rolExiste) return rolExiste
-    const newRole = await roleRepository.roleCreate({ name_rol: role.name_role })
+export const roleServices = {
+  createUserRole: async (role: { id_role: string, id_user: string, name_role: string }) => {
+    const userExiste = await userRepository.findIdUser(role.id_user)
+    if (!userExiste) throw new Error('Error al encontrar el usuario por el id')
 
-    return newRole
+    let roleExiste = await roleRepository.findRoleName(role.name_role)
+    if (!roleExiste) return roleExiste = await roleRepository.createRole({ name_role: role.name_role })
+
+    const newUserRole = await roleRepository.createUserRole({
+      id_role: roleExiste.id_role,
+      id_user: role.id_user
+    })
+
+    return newUserRole
   }
 }
