fcc8f3506d26e21cf68a3ee1f112dccf0ea506c0
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-07-28 17:25:36 -0400

    feat: agregar manejo de caché para comentarios y likes utilizando Redis

--- Backend/src/socket/like-comment.socket.ts
+++ Backend/src/socket/like-comment.socket.ts
@@ -1,11 +1,26 @@
 import { Server, Socket } from 'socket.io'
 import { likeService } from '../services/likes.service'
 import { formatError } from '../utils/error.utils'
+import { redis } from '../config/redis'
+import { saveCache, verifyCache } from '../utils/cache.utils'
 
 export const socketLikeComment = async (socket: Socket, io: Server) => {
   socket.on('likeComment', async (data) => {
     try {
+      const key = `likeComment:${data.id_comment}`
+      const cache = await redis.get(key)
+
+      const newLikeCache = verifyCache(cache)
+      if (newLikeCache !== null) {
+        io.emit('newLikeCache', newLikeCache)
+        return
+      }
+
       const newLike = await likeService.likePushComment(data)
+      const { options, value } = saveCache(newLike)
+
+      await redis.set(key, value, options)
+
       io.emit('newLikeComment', newLike)
     } catch (error) {
       socket.emit('errorEmitLikeComment', {
--- Backend/src/socket/like.socket.ts
+++ Backend/src/socket/like.socket.ts
@@ -1,11 +1,26 @@
 import { Server, Socket } from 'socket.io'
 import { likeService } from '../services/likes.service'
 import { formatError } from '../utils/error.utils'
+import { redis } from '../config/redis'
+import { saveCache, verifyCache } from '../utils/cache.utils'
 
 export const socketLike = (socket: Socket, io: Server) => {
   socket.on('like', async (data) => {
     try {
+      const key = `like:${data.id_post}`
+      const cache = await redis.get(key)
+
+      const newLikeCache = verifyCache(cache)
+      if (newLikeCache !== null) {
+        io.emit('newLikeCache', newLikeCache)
+        return
+      }
+
       const newLike = await likeService.likePush(data)
+      const { options, value } = saveCache(newLike)
+
+      await redis.set(key, value, options)
+
       io.emit('NewLike', newLike)
     } catch (error) {
       socket.emit('ErrorEmitLike', {
