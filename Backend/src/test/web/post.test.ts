8855e2fa70c4bccf04cd5602a9f9bd80c517228b
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-08-05 17:45:31 -0400

    post test for jest

--- Backend/src/test/web/post.test.ts
+++ Backend/src/test/web/post.test.ts
@@ -11,13 +11,54 @@ jest.mock('../../config/redis', () => ({
 const title = 'Microsoft saco algo nuevo'
 const content = 'microsfot saco una nuva forma de aprender Python'
 const id_author = '6de6b4b0-82af-4c20-bd55-797654189478'
+const url = `/api/user/create-post/${id_author}`
 
 describe('POST /api/user/create-post/:id', () => {
   test ('Se espero que el codigo de estado sea un 201', async () => {
-    const response = await request(app).post(`/api/user/create-post/${id_author}`).send({
+    const response = await request(app).post(url).send({
       title, content
     })
 
     expect(response.statusCode).toBe(201)
+  })
+
+  test('se espera que lo enviado sea un object', async () => {
+    const response = await request(app).post(url).send({
+      title, content
+    })
+
+    expect(typeof response.body).toBe('object')
+  })
+
+  test('Se espera que lo enviado sea un Content-type application/json', async () => {
+    const response = await request(app).post(url).send({
+      title, content
+    })
+
+    expect(response.headers).toHaveProperty('json')
+  })
+
+  test('Se espera que lo enviado no este vacio', async () => {
+    const response = await request(app).post(url).send({
+      title: '', content: ''
+    })
+
+    expect(response.statusCode).toBe(500)
+  })
+
+  test('Se espera que el title no este vacio', async () => {
+    const response = await request(app).post(url).send({
+      title: '', content: 'dasdasda'
+    })
+
+    expect(response.statusCode).toBe(500)
+  })
+
+  test('Se espera que el content no este vacio', async () => {
+    const response = await request(app).post(url).send({
+      title: 'dasd', content: ''
+    })
+
+    expect(response.statusCode).toBe(500)
   })
 })
